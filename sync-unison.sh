b1ac07a88e6c135dc74c15eca198828e05d97645
maxfaber <maxfaber@quicknet.nl>
2022-10-11 21:49:34 +0200

    Removed sync-unison.sh

--- sync-unison.sh
+++
@@ -1,4 +0,0 @@
-CUR_PATH=$PWD
-CUR_DIR=${PWD##*/}
-
-unison -auto -batch -repeat watch -times "$CUR_PATH" ssh://nas/Drive/code/"$CUR_DIR" -ignore "Name .DS_Store" -ignore "Name @eaDir" -prefer newer -copyonconflict
